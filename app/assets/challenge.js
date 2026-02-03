b161c1135f285482fba42951693da5f0892ccbe2
Ascendise <ascendise@ascendise.ch>
2026-02-03 19:10:49 +0000

    fix: page not rendering on Chromium (#97)
    Closes #96
    
    Main culprit was the fact that the stamp calculation was blocking the rendering thread. While Firefox/Gecko seems to "make it work", Chromium fully stops and just does not let the page render fully because it is occupied with calculating stamps. Moving the stamp calculation into a web worker solved this.
    
    Reviewed-on: https://gitea.ascendise.ch/ascendise/tollkeeper/pulls/97
    Co-authored-by: Ascendise <ascendise@ascendise.ch>
    Co-committed-by: Ascendise <ascendise@ascendise.ch>

--- app/assets/challenge.js
+++ app/assets/challenge.js
@@ -1,69 +1,43 @@
-async function isValidStamp(stamp, bits) {
-  let hash = await window.crypto.subtle.digest("SHA-1", new TextEncoder().encode(stamp));
-  bytes = new Uint8Array(hash);
-  let bits_left = bits;
-  console.log(stamp);
-  for (const obyte of bytes) {
-    let expected_bits = 8 <= bits_left ? 8 : bits_left;
-    console.log(`expected: ${expected_bits}`)
-    let valid = 0 == (obyte >> (8 - expected_bits));
-    console.log(`valid: ${valid}`)
-    if(!valid) {
-      return false;
-    }
-    bits_left -= expected_bits;
-    if(bits_left == 0) {
-      console.log(`Valid stamp found: ${stamp}`)
-      return true;
+async function main() {
+  const tollHalJson = document.getElementById('toll').innerHTML;
+  const tollHal = JSON.parse(tollHalJson);
+  const worker = await startWorker();
+  worker.postMessage([JSON.stringify(tollHal.toll.challenge)])
+  const currentStamp = document.getElementById('stamp');
+  worker.onmessage = (e) => {
+    if (e.data[0]) {
+      currentStamp.innerHTML = e.data[0];
+    } else {
+      payToll(tollHal, e.data[1])
     }
   }
 }
 
-async function calcStamp(stamp_prefix, bits) {
-  let stamp_el = document.getElementById('stamp');
-  let count = 0;
-  let stamp;
-  do {
-    stamp = `${stamp_prefix}:${count}`
-    stamp_el.innerHTML = stamp;
-    count++;
-  } while (!await isValidStamp(stamp, bits))
-  return stamp;
+async function startWorker() {
+  const workerUrl = document.getElementById('workerScript').href;
+  const workerRes = await fetch(workerUrl);
+  const workerJs = await workerRes.text();
+  const workerBlob = new Blob([workerJs], {
+    type: "text/javascript"
+  });
+  return new Worker(URL.createObjectURL(workerBlob));
 }
 
-function padTime(timeElement) {
-  return timeElement.toString().padStart(2, "0");
-}
-
-async function main() {
-  const toll_json = document.getElementById('toll').innerHTML;
-  const toll_res = JSON.parse(toll_json);
-  const date = new Date();
-  const year = padTime(date.getUTCFullYear() % 100);
-  const month = padTime(date.getUTCMonth() + 1);
-  const day = padTime(date.getUTCDate());
-  const hours = padTime(date.getUTCHours());
-  const minutes = padTime(date.getUTCMinutes());
-  const seconds = padTime(date.getUTCSeconds());
-  const date_string = `${year}${month}${day}${hours}${minutes}${seconds}`;
-  const rand = window.crypto.randomUUID().replace(/-/g, "");
-  const toll = toll_res.toll;
-  const challenge = toll.challenge;
-  const stamp_prefix = `${challenge.ver}:${challenge.bits}:${date_string}:${challenge.resource}:${challenge.ext}:${rand}`;
-  const stamp = await calcStamp(stamp_prefix, challenge.bits)
+async function payToll(tollHal, stamp) {
   const payment = {
-    toll: toll,
+    toll: tollHal.toll,
     value: stamp
   };
-  const pay_link = toll_res._links.pay;
-  const response = await fetch(pay_link, {
+  const payLink = tollHal._links.pay;
+  const response = await fetch(payLink, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(payment)
   });
-  let visa = await response.json();
+  const visa = await response.json();
   document.cookie = `${visa.header_name}=${visa.token}; path=/`;
   window.location.href = `http://${visa._links.origin_url}`;
+
 }
 
 main();
